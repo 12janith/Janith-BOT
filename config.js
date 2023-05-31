050e311b975f58d3d4a43a120da9c41629163b49
12janith <134947799+12janith@users.noreply.github.com>
2023-05-31 20:55:07 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['94764497079', 'sandaru', true],
-  ['6281255369012', 'Ash', true], 
+  ['94777338157', 'sandaru', true],
+  ['94777338157', 'Janiya', true], 
   [''] 
 ] //Number of owners
 
